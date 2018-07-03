05aea52d35362df6dcadc727e99fe2be5938efb1
Stud <Stud@192.168.16.109>
2018-07-03 12:35:27 +0200

    added tests on publisher

--- thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/TestPublisher.java
+++ thelibrary-core/src/test/java/com/benfante/javacourse/thelibrary/core/model/TestPublisher.java
@@ -11,6 +11,40 @@ public class TestPublisher{
 		publisher = new Publisher(id , name);
 		assertEquals(publisher.getName() , name);
 		assertEquals(publisher.getId() , id); 
-		
+	}
+	
+	@Test
+	public void testSets() {
+		Publisher publisher;
+		String name = "test";
+		long id = 2;
+		publisher = new Publisher(10,"hey");
+		publisher.setId(id);
+		publisher.setName(name);
+		assertEquals(publisher.getId() , id);
+		assertEquals(publisher.getName() , name);
+	}
+	
+	@Test
+	public void testToString() {
+		Publisher publisher;
+		String name = "test";
+		long id = 2;
+		publisher = new Publisher(id , name);
+		String str = publisher.toString();
+		String compar = "Id: " +id+ "\nName: "+name;
+		assertEquals(str , compar);
+	}
+	
+	@Test
+	public void testEquals() {
+		Publisher publisher;
+		String name = "test";
+		long id = 2;
+		publisher = new Publisher(id , name);
+		boolean tr = publisher.equals(new Publisher(id , name));
+		boolean fls = publisher.equals(new Publisher(5 , "notname"));
+		assertEquals( tr , true );
+		assertEquals( fls , false );
 	}
 }

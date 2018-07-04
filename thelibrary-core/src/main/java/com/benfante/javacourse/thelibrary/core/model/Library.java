834e9db8f43b2bd4eba85573263059c9ef11e25e
Stud <Stud@192.168.16.103>
2018-07-04 15:28:17 +0200

    removed main

--- thelibrary-core/src/main/java/com/benfante/javacourse/thelibrary/core/model/Library.java
+++ thelibrary-core/src/main/java/com/benfante/javacourse/thelibrary/core/model/Library.java
@@ -9,87 +9,11 @@ public class Library {
 	static BookCompare bec;
 	static Scanner in = new Scanner(System.in);
 
-//----------------------------------------MAIN-------------------------------------------------
 	public static void main(String[] args) {
-		Library library = new Library();
-		int sw = 0,sw2 = 0;;
-		
-		do {
-			System.out.println("\n\nINSERISCI UN NUMERO PER CONTINUARE:\n(1)Inserisci un libro\n(2)Rimuovi un libro\n(3)Stampa tutti i libri\n(4)Ricerca per singolo campo\n(0)ESCI");
-			sw = in.nextInt();
-			in.nextLine();
-			switch(sw) {
-				case 1:library.addBook(insertBook());
-					   break;
-					   
-				case 2:library.removeBook(insertBook());
-				       break;
-				       
-				case 3:if(library.books.length > 0) {
-							for(int i = 0;i<library.books.length;i++) {
-								if(library.books[i] != null) {
-								System.out.println(library.books[i].toString()+"\n");
-								}else {
-									break;
-								}
-							 }
-							break; 
-					   }
-					   System.out.println("Nessun libro da stampare.");
-					   break;
-					   
-				case 4:System.out.println("Ricerca tramite:");
-					   System.out.println("(1)ID\n(2)Titolo\n(3)Autore/i\n(4)Prezzo\n(5)Publisher\n");
-					   sw2 = in.nextInt();
-					   in.nextLine();
-					   switch(sw2) {
-					   		case 1:	System.out.println("Inserisci l'Id da cercare:\n");
-					   				int id = 0;
-					   				id = in.nextInt();
-					   				in.nextLine();					   		
-					   				library.searchBooksById(id);
-					   				break;
-					   		case 2:System.out.println("Inserisci il titolo da cercare:\n");
-			   					   String title = "";
-			   					   title = in.nextLine();			   		
-			   					   library.searchBooksByTitle(title);
-			   					   break;
-					   		case 3:System.out.println("Inserisci l'autore da cercare:\n");
-					   			   System.out.print("nome:");
-					   			   String name = in.nextLine();
-					   			   System.out.print("cognome:");
-					   			   String lastName = in.nextLine();
-					   			   System.out.println();
-		   					   	   Author[] author = new Author[1];
-		   					   	   author[0]= new Author((int)(Math.random()*5)+1,name,lastName);
-		   					       library.searchBooksByAuthor(author);
-		   					       break;
-					   		case 4:System.out.println("Inserisci il prezzo da cercare:\n");
-		   					       float price;
-		   					       price = in.nextFloat();	
-		   					       in.nextLine();
-		   					       library.searchBooksByPrice(price);
-		   					       break;
-					   		case 5:System.out.println("Inserisci il publisher da cercare:\n");
-					   		       Publisher publisher;
-					   			   System.out.println("Inserisci publisher");
-					   			   publisher = new Publisher(1, in.nextLine());
-	   					           library.searchBooksByPublisher(publisher);
-	   					           break;
-					   }
-					   break;
-				case 0:
-					   System.out.println("CIAO!");
-					   break;
-				default:
-					   System.out.println("Inserimento non valido!");
-					   break;
-			}
-		   }while(sw != 0); 	
+	
 		in.close();
 	}	
 
-//----------------------------------------ADD BOOK------------------------------------------------------
 	public  void addBook(Book book) {
 		log.debug("\nAdding book with:\ntitle = {}\nid = {}",book.getTitle() , book.getId() );
 		Book[] copy;
@@ -112,7 +36,7 @@ public class Library {
 		}
 		
 	}
-//-----------------------------------------REMOVE BOOK--------------------------------------------------------
+
 	public  void removeBook(Book book) {
 		boolean removed = false;
 		Book[] copy = null;
@@ -145,36 +69,7 @@ public class Library {
 		}
 	}
 //---------------------------------------INSERIMENTO TASTIERA LIBRO----------------------------------------------------------------------------
-	public  static Book insertBook() {
-		   int id;
-		   Author author;
-		   Book b;
-		   String publisherN;
-		   String title,authorN,authorC;
-		   float price;
-		   Publisher publisher;
-		   System.out.println("Inserisci Id: ");
-	       id = in .nextInt();
-		   System.out.println("Inserisci un titolo:");
-	       title = in.nextLine();
-	       title = in.nextLine();
-	       System.out.println("Inserisci nome autore");
-	       authorN = in.nextLine();
-	       System.out.println("Inserisci cognome autore");
-	       authorC = in.nextLine();
-	       author = new Author((int)(Math.random()*5)+1,authorN, authorC);
-	       System.out.println("Inserisci prezzo:");
-	       price = in.nextFloat();
-	       in.nextLine();
-	       System.out.println("Inserici publisher:");
-	       publisherN = in.nextLine();
-	       publisher = new Publisher(1,publisherN);
-	       b = new Book (id,title,author,price,publisher);
-	       return b;
-	       
-	}
 	
-//------------------------------------------RICERCHE-----------------------------------------------------
 	public   Book[] searchBooksById(int id) {
 		Book[] book = new Book[books.length];
 		int ct = 0;
@@ -192,7 +87,6 @@ public class Library {
 		System.out.println();
 		return book;
 	}
-	
 	
 	
 	public   Book[] searchBooksByTitle(String title) {
@@ -213,7 +107,6 @@ public class Library {
 	}
 	
 	
-	
 	public   Book[] searchBooksByAuthor(Author[] author) {
 		Book[] book = new Book[books.length];
 		int ct = 0;
@@ -231,8 +124,7 @@ public class Library {
 		return book;
 	}
 	
-	
-	
+		
 	public   Book[] searchBooksByPrice(float price) {
 		Book[] book = new Book[books.length];
 		int ct = 0;
@@ -269,7 +161,7 @@ public class Library {
 		return book;
 	}
 	
-//-------------------------------------------------------------------------------------------------
+
 	
 	
 }
